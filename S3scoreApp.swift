8982279509a911a0c70fc939cc04d54187e870a5
Mikael de Lorenzi Tognon <104089875+metg1985@users.noreply.github.com>
2023-12-03 00:32:40 -0800

    Add files via upload

---
+++ S3scoreApp.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+@main
+struct S3scoreApp: App {
+    @StateObject private var modelData = ModelData()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(modelData)
+        }
+    }
+}
